cbe78fec17183c5bcb8c833f5c4aa0752e4396f1
rgs19 <rgs19@psu.edu>
2017-04-21 20:30:19 -0400

    Added chat b/w client & server.

---
+++ src/battleboats/ChatClient.java
@@ -0,0 +1,79 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleboats;
+
+import java.io.*;
+import java.net.*;
+import java.util.Scanner;
+
+/**
+ *
+ * @author rgs19
+ */
+
+public class ChatClient {
+
+	private String host;
+	private int port;
+
+	public static void main(String[] args) throws IOException {
+            
+                // 98.114.8.244 - main server
+                // USE 127.0.0.1 if working on this while main server is not up
+                
+		new ChatClient("127.0.0.1", 9999).run();
+	}
+
+	public ChatClient(String IP, int port) {
+		this.host = host;
+		this.port = port;
+	}
+
+	public void run() throws IOException {
+            
+		Socket client = new Socket(host, port);
+		System.out.println("Connected to BattleBoats server!");
+
+		
+		new Thread(new IncomingMessagesHandler(client.getInputStream())).start();
+
+                String username = "testuser"; //just added this for now, wasn't sure what the best way would be to fetch the username from LoginPanel - RGS
+		
+		System.out.print("User " + username + " has joined the chat");
+	
+		
+		System.out.println("[Please type your message:]");
+		
+                Scanner cc = new Scanner(System.in);
+                PrintStream output = new PrintStream(client.getOutputStream());
+                while (cc.hasNextLine()) {
+			output.println(username + ": " + cc.nextLine());
+		}
+		
+		output.close();
+		cc.close();
+		client.close();
+	}
+}
+
+class IncomingMessagesHandler implements Runnable {
+
+	private InputStream server;
+
+	public IncomingMessagesHandler(InputStream server) {
+		this.server = server;
+	}
+
+	@Override
+	public void run() {
+		// receive server messages and print them
+		Scanner s = new Scanner(server);
+		while (s.hasNextLine()) {
+			System.out.println(s.nextLine());
+		}
+		s.close();
+	}
+}
---
+++ src/battleboats/ChatServer.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package battleboats;
+
+/**
+ *
+ * @author rgs19
+ */
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class ChatServer {
+
+	private int port;
+	private List<PrintStream> clients;
+	private ServerSocket server;
+
+	public static void main(String[] args) throws IOException {
+		new ChatServer(9999).run();
+	}
+
+	public ChatServer(int port) {
+		this.port = port;
+		this.clients = new ArrayList<PrintStream>();
+	}
+
+	public void run() throws IOException {
+		server = new ServerSocket(port) {
+			protected void finalize() throws IOException {
+				this.close();
+			}
+		};
+		System.out.println("Port 9999 is now open.");
+
+		while (true) {
+			// accepts connection to a client
+			Socket client = server.accept();
+			System.out.println("Connection established with client: " + client.getInetAddress().getHostAddress());
+			
+			// add client message to list
+			this.clients.add(new PrintStream(client.getOutputStream()));
+			
+			// new thread creation for client handling
+			new Thread(new ClientHandler(this, client.getInputStream())).start();
+		}
+	}
+
+	void broadcastMessages(String msg) {
+		for (PrintStream client : this.clients) {
+			client.println(msg);
+		}
+	}
+}
+
+class ClientHandler implements Runnable {
+
+	private ChatServer server;
+	private InputStream client;
+
+	public ClientHandler(ChatServer server, InputStream client) {
+		this.server = server;
+		this.client = client;
+	}
+
+	@Override
+	public void run() {
+		String message;
+		
+		// when there is a new message, broadcast to all
+		Scanner cs = new Scanner(this.client);
+		while (cs.hasNextLine()) {
+			message = cs.nextLine();
+			server.broadcastMessages(message);
+		}
+		cs.close();
+	}
+}
